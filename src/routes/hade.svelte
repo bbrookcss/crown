8b15ad4842e8005dfc54f4c3fa992c4c406fb59f
bbrookcss <bbrookyotube@gmail.com>
2024-08-02 16:56:40 +0300

    login for fast

--- src/routes/hade.svelte
+++ src/routes/hade.svelte
@@ -13,7 +13,7 @@
             return;
         }
 
-        const response = await fetch('http://localhost:3000/users/login', {
+        const response = await fetch('https://backend.crownweddingfilms.com/users/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
